d0d43233fd2030e057e6ff3c2d82ff993435026f
Britta Grusdt <bgrusdt@gmail.com>
2023-02-04 12:12:55 +0100

    refactor data loading code to functions

---
+++ R/helpers-load-data.R
@@ -0,0 +1,76 @@
+# load likelihood parameters fitted to data
+get_likelihood_params_fitted_data = function(result_dir, params){
+  
+  dep_data_dir = paste(result_dir, "dependent-contexts", sep=FS)
+  ind_data_dir = paste(result_dir, "independent-contexts", sep=FS)
+  
+  pars.dep <- readRDS(paste(dep_data_dir, "evs-posterior-dependent-data.rds", sep=FS)) %>% 
+    pivot_longer(cols=c(-id), names_to = "p", values_to = "value") %>% 
+    mutate(p = str_replace(p, "if_", "if")) %>% 
+    separate(p, into = c("param", "p"), sep="_") %>% 
+    mutate(p = str_replace(p, "if", "if_")) %>% 
+    pivot_wider(names_from = "param", values_from = "value")
+  
+  pars.ind <- readRDS(paste(ind_data_dir, "evs-posterior-independent-data.rds", sep=FS)) %>% 
+    dplyr::select(-theta_p) %>% 
+    pivot_longer(cols=c(-id), names_to = "p", values_to = "value") %>% 
+    separate(p, into = c("param", "p"), sep="_") %>% 
+    pivot_wider(names_from = "param", values_from = "value")
+  
+  # noise added vs.subtracted to P(b)*P(g)
+  pars.ind.theta_p <- readRDS(
+    paste(ind_data_dir, "evs-posterior-independent-data.rds", sep=FS)
+  ) %>% dplyr::select(id, theta_p) %>%
+    rename(theta = theta_p)
+  
+  pars = list(likelihoods_zoib = bind_rows(pars.dep, pars.ind),
+              likelihoods_bernoulli = pars.ind.theta_p)
+  return(pars)
+}
+
+
+draw_states_from_prior = function(params){
+  prior <- webppl(program_file = here("webppl-model", "run-state-prior.wppl"),
+                  data_var = "data",
+                  data = params,
+                  packages = c("webppl-model/node_modules/conditionalsHelpers", 
+                               "webppl-model/node_modules/conditionalsDefault")) 
+  states = prior$prior$support %>% as_tibble()
+  return(states)
+}
+
+get_rsa_states = function(params){
+  states <- draw_states_from_prior(params)
+  pars <- list(prior_samples = states)
+  return(pars)
+}
+
+get_observed_data = function(data.behav, params){
+  data.uc = data.behav %>%  filter(!is.na(uc_task)) %>% 
+    dplyr::select(prolific_id, id, utterance) %>% 
+    add_column(n = 1) %>% group_by(id, utterance)
+  data.pe = data.behav %>% 
+    dplyr::select(prolific_id, id, utt.standardized, pe_task) %>% 
+    pivot_wider(names_from = "utt.standardized", values_from = "pe_task") %>% 
+    rename(AC = `both blocks fall`, 
+           `A-C` = `blue falls but green does not fall`, 
+           `-AC` = `green falls but blue does not fall`, 
+           `-A-C` = `neither block falls`)
+  # all subjects and trials
+  data.observed = left_join(data.uc, data.pe)
+  
+  # observed ratios per context
+  contexts <- data.behav$id %>% unique()
+  ratios.observed <- left_join(
+    tibble(id = contexts, utterance = list(params$utterances)) %>%
+      unnest(c(utterance)),
+    data.observed %>% group_by(id, utterance) %>% count(), 
+  ) %>% mutate_if(is.numeric, coalesce, 0) %>% 
+    group_by(id) %>% 
+    mutate(ratio = n / sum(n))
+  
+  pars <- list(observations = data.observed, 
+               observed_utts_ratios = ratios.observed)
+  return(pars)
+}
+
--- R/single-model-run.R
+++ R/single-model-run.R
@@ -6,6 +6,7 @@ library(ExpDataWrangling)
 library(ModelUtils)
 
 source(here("R", "plot-functions.R"))
+source(here("R", "helpers-load-data.R"))
 
 theme_set(theme_minimal(base_size=20) + theme(legend.position = "top"))
 
@@ -16,14 +17,6 @@ Sys.setenv(R_CONFIG_ACTIVE = active_config)
 
 params <- config::get()
 if(!dir.exists(params$dir_results)) dir.create(params$dir_results, recursive = T)
-
-dep_data_dir = paste(here(params$dir_results), "dependent-contexts", sep=FS)
-ind_data_dir = paste(here(params$dir_results), "independent-contexts", sep=FS)
-
-# Data
-Sys.setenv(R_CONFIG_ACTIVE = "cleaned_data")
-params.data = config::get()
-
 # use special causal nets, with different marginal distributions than uniform
 # cns = create_causal_nets(params$p_noise, params$p_cp, params$p_ant,
 #                          params$rels_dep, params$p_a, params$p_c)
@@ -35,67 +28,20 @@ data.behav <- read_csv(here(params$dir_data, "cleaned-data.csv")) %>%
   dplyr::select(prolific_id, id, utt.standardized, uc_task, pe_task, slider) %>% 
   translate_standardized2model() 
 
-data.uc = data.behav %>%  filter(!is.na(uc_task)) %>% 
-  dplyr::select(prolific_id, id, utterance) %>% 
-  add_column(n = 1) %>% group_by(id, utterance)
-data.pe = data.behav %>% 
-  dplyr::select(prolific_id, id, utt.standardized, pe_task) %>% 
-  pivot_wider(names_from = "utt.standardized", values_from = "pe_task") %>% 
-  rename(AC = `both blocks fall`, 
-         `A-C` = `blue falls but green does not fall`, 
-         `-AC` = `green falls but blue does not fall`, 
-         `-A-C` = `neither block falls`)
-
-data.observed = left_join(data.uc, data.pe)
-params$observations = data.observed
-
-
+# add parameters to run webppl model
+# observed data each participant and trial + ratios
+pars.observations <- get_observed_data(data.behav, params)
 # draw samples from prior
-prior <- webppl(program_file = here("webppl-model", "run-state-prior.wppl"),
-                data_var = "data",
-                data = params,
-                packages = params$packages[1:2]) 
-states = prior$prior$support %>% as_tibble()
-params$prior_samples = states
-params$p_utts = rep(1 / length(params$utterances), length(params$utterances))
-
+pars.rsa_states <- get_rsa_states(params)
 # load likelihood parameters fitted to data
-pars.dep <- readRDS(paste(dep_data_dir, "evs-posterior-dependent-data.rds", sep=FS)) %>% 
-  pivot_longer(cols=c(-id), names_to = "p", values_to = "value") %>% 
-  mutate(p = str_replace(p, "if_", "if")) %>% 
-  separate(p, into = c("param", "p"), sep="_") %>% 
-  mutate(p = str_replace(p, "if", "if_")) %>% 
-  pivot_wider(names_from = "param", values_from = "value")
-
-pars.ind <- readRDS(paste(ind_data_dir, "evs-posterior-independent-data.rds", sep=FS)) %>% 
-  dplyr::select(-theta_p) %>% 
-  pivot_longer(cols=c(-id), names_to = "p", values_to = "value") %>% 
-  separate(p, into = c("param", "p"), sep="_") %>% 
-  pivot_wider(names_from = "param", values_from = "value")
-
-# noise added vs.subtracted to P(b)*P(g)
-pars.ind.theta_p <- readRDS(
-  paste(ind_data_dir, "evs-posterior-independent-data.rds", sep=FS)
-) %>% dplyr::select(id, theta_p) %>%
-  rename(theta = theta_p)
-
-params$likelihoods_zoib <- bind_rows(pars.dep, pars.ind)
-params$likelihoods_bernoulli <- pars.ind.theta_p
+pars.likelihoods <- get_likelihood_params_fitted_data(here(params$dir_results), params)
+# if not provided, all utterances equally likely
+# params$p_utts = rep(1 / length(params$utterances), length(params$utterances))
+params <- c(params, pars.observations, pars.likelihoods, pars.rsa_states)
 
 # Run Model ---------------------------------------------------------------
 # 1. predictions by contexts
 path_model_file = paste(params$dir_wppl_code, "model-single-run-by-contexts.wppl", sep=FS)
-
-# include observations as ratios and counts
-trials <- data.pe$id %>% unique()
-params$observed_utts_ratios <- left_join(
-    tibble(id = trials, utterance = list(params$utterances)) %>%
-      unnest(c(utterance)),
-    params$observations %>% group_by(id, utterance) %>% count(), 
-  ) %>% mutate_if(is.numeric, coalesce, 0) %>% 
-  group_by(id) %>% 
-  mutate(ratio = n / sum(n))
-
 params$packages <- c(params$packages, paste("webppl-model", "node_modules", 
                                             "dataHelpers", sep = FS))
 posterior <- run_webppl(path_model_file, params)
@@ -142,10 +88,9 @@ production.joint.dij = left_join(
 # average prediction across all participants
 model.avg <- production.joint.dij %>% group_by(id, utterance) %>% 
   summarize(model = mean(model), .groups = "drop_last")
-behav.avg <- data.uc %>% 
-  group_by(id, utterance) %>%
-  summarize(behavioral = n(), .groups = "drop_last") %>% 
-  mutate(behavioral = behavioral / sum(behavioral))
+behav.avg <- pars.observations$observed_utts_ratios %>%
+  rename(behavioral = ratio) %>% 
+  dplyr::select(id, utterance, behavioral)
 joint.avg.dij <- left_join(model.avg, behav.avg) %>% 
   mutate(behavioral = case_when(is.na(behavioral) ~ 0, T ~ behavioral))
 
