b2ad723c4f078fac4765ec57fba18d68bf4ad09e
Britta Grusdt <bgrusdt@gmail.com>
2023-02-23 11:45:43 +0100

    added model vs data barplot

--- R/plot-functions.R
+++ R/plot-functions.R
@@ -1,4 +1,6 @@
 library(ggpubr)
+names_data <- c(`behavioral` = "darkgreen", `model` = "firebrick")
+
 plot_correlation = function(results.joint, 
                             color = "utterance", shape = "relation",
                             label.x = 0.1, label.y = NA){
@@ -18,4 +20,28 @@ plot_correlation = function(results.joint,
   }
 
   return(p_scatter)
+}
+
+
+plot_model_vs_data_bars = function(df.joint, tit = "", by_utt_type=F){
+  df.long <- df.joint %>%
+    pivot_longer(cols = c("model", "behavioral"), names_to = "data",
+                 values_to = "probability")
+  if(by_utt_type){
+    df.long <- df.long %>% mutate(utt=utterance) %>% chunk_utterances() %>% 
+      rename(utt_type = utterance, utterance = utt) %>% 
+      group_by(id, relation, data, utt_type) %>% 
+      summarize(probability = sum(probability), .groups = "drop_last") %>% 
+      rename(utterance = utt_type)
+  }
+  plots = map(df.long$relation %>% unique(), function(rel){
+    p <- df.long %>% filter(relation == !!rel) %>% 
+      ggplot(aes(y=utterance, x=probability, fill=data)) + 
+      geom_bar(stat="identity", position = position_dodge()) +
+      labs(x = "condition", title = tit) + 
+      facet_wrap(~id, scales = "free") +
+      scale_fill_manual(values = names_data)
+    return(p)
+  })
+  return(plots)
 }
