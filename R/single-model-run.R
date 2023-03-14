97f8eaa63d263e9846fe943d3522e1cbb9f710a3
Britta Grusdt <bgrusdt@gmail.com>
2023-03-14 14:37:59 +0100

    added code to make rsa prediction based on N samples from Weights

--- R/single-model-run.R
+++ R/single-model-run.R
@@ -17,12 +17,20 @@ Sys.setenv(R_CONFIG_ACTIVE = active_config)
 
 params <- config::get()
 if(!dir.exists(params$dir_results)) dir.create(params$dir_results, recursive = T)
-# use special causal nets, with different marginal distributions than uniform
-# cns = create_causal_nets(params$p_noise, params$p_cp, params$p_ant,
-#                          params$rels_dep, params$p_a, params$p_c)
-# params$causal_nets_dep = cns$dep
-# params$causal_nets_ind = cns$ind
 
+################################################################################
+# use more fine-grained causal nets (causal power, noise: low/unc/high)
+active_config = "fine_grained_causal_nets"
+Sys.setenv(R_CONFIG_ACTIVE = active_config)
+pars.cns <- config::get()
+cns = create_causal_nets(pars.cns$dep_causal_power, pars.cns$dep_noise, 
+                         pars.cns$dep_marginal, params$rels_dep,
+                         pars.cns$ind_a, pars.cns$ind_c)
+# dont allow causal power to be lower category than noise
+params$causal_nets_dep = cns$dep[!str_detect(cns$dep, "-low-") &
+                                   !str_detect(cns$dep, "-unc-high")]
+params$causal_nets_ind = cns$ind
+################################################################################
 # behavioral data
 data.behav <- read_csv(here(params$dir_data, "cleaned-data.csv")) %>% 
   dplyr::select(prolific_id, id, utt.standardized, uc_task, pe_task, slider) %>% 
@@ -33,6 +41,11 @@ data.behav <- read_csv(here(params$dir_data, "cleaned-data.csv")) %>%
 pars.observations <- get_observed_data(data.behav, params)
 # draw samples from prior
 pars.rsa_states <- get_rsa_states(params)
+# sanity check:
+pars.rsa_states$prior_samples %>% 
+  group_by(probability) %>% dplyr::count() %>% ungroup() %>%
+  mutate(proportion = n / sum(n), N=sum(n))
+
 # load likelihood parameters fitted to data
 pars.likelihoods <- get_likelihood_params_fitted_data(params)
 # if not provided, all utterances equally likely
@@ -47,14 +60,14 @@ params$packages <- c(params$packages, paste("webppl-model", "node_modules",
 active_config = "priors_relations"
 Sys.setenv(R_CONFIG_ACTIVE = active_config)
 par_relations <- config::get()
-params$prior_relations <- par_relations[["informative"]]
+params$prior_relations <- par_relations[["uninformative"]]
 params$utt_cost <- tibble(utterance = params$utterances, cost=params$utt_cost)
 
 params$alpha <- 1.98
 params$theta <- 0.764
 
-params$alpha <- 2.08
-params$theta <- 0.699
+params$alpha <- 2.58
+params$theta <- 0.687
 # params$gamma <- 1
 # params$utt_cost <- df.p_utts %>% dplyr::select(-p) %>% 
 #   pivot_wider(names_from="Parameter", values_from="value")
@@ -69,11 +82,37 @@ data <-   webppl(program_code = model,
                  packages = params$packages
 )
 posterior <- data %>% map(function(x){as_tibble(x)})
+
+###############################################################################
+# new prediction based on repeatedly drawn N_participants states from 
+# weights to make rsa predictions
+model.predictions <- posterior %>% bind_rows() %>% rowid_to_column() %>% 
+  unnest(c(p_hat, utterance)) %>% 
+  mutate(ll_ci = as.numeric(ll_ci)) %>% 
+  arrange(id, p_hat)
+#hdis.mean = mean_hdi(model.predictions %>% group_by(id, utterance), p_hat)
+pred.quantiles = group_map(model.predictions %>% group_by(id, utterance), 
+                           function(df, df.grp){
+  qs = quantile(df$p_hat, c(0.025, 0.5, 0.975))
+  tibble(estimate=qs[['50%']], lower=qs[['2.5%']], upper = qs[['97.5%']], 
+         id=df.grp$id, 
+         utterance=df.grp$utterance)
+}) %>% bind_rows()
+model.predictions %>% filter(str_detect(id, "if1_uh")) %>% 
+  ggplot(aes(x=p_hat, fill=id)) + 
+  geom_histogram(alpha=0.5, position = 'identity', binwidth=0.01) +
+  facet_wrap(~utterance, scales="free_y", ncol=4) +
+  theme(axis.text.x = element_text(size=8)) +
+  scale_fill_brewer(name = "context", palette = "Set1") +
+  labs(x = "predicted probability")
+###############################################################################
+
 model.predictions <- posterior %>% 
   map(function(x){as_tibble(x) %>% mutate(ll_ci = as.numeric(ll_ci))}) %>% 
   bind_rows() %>% group_by(id) %>% 
   mutate(p_hat_round = round(p_hat, 2)) %>% 
   arrange(desc(p_hat))
+
 
 production.joint = left_join(model.predictions, 
                              params$observed_utts_ratios %>% rename(p = ratio)) %>% 
